eb2a243cee52398c37199da7df5bab195bc8d692
hasan sbeity <th.rider.clan@gmail.com>
2025-12-19 18:52:23 +0200

    deployement setup 2

--- server/app.js
+++ server/app.js
@@ -11,10 +11,30 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(bodyParser.json());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://urbanfitx.com",
+      "https://api.urbanfitx.com",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "X-Auth-Token",
+    ],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 86400, // Cache for Preflight-requests for 24 hours (86400 sec)
+  })
+);
 
 // Multer setup for file upload
 const upload = multer({ dest: "uploads/" }); // Store files temporarily
